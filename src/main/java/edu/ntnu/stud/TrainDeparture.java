54cbdf55867b8f89b1fdb7ff50eafad55c0abb43
florianleucht <fsleucht@stud.ntnu.no>
2023-10-28 14:24:55 +0200

    added Getters and setters

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -14,6 +14,14 @@ public class TrainDeparture {
   private int track;
   private LocalTime delay = LocalTime.of(0, 0);
 
+  /**
+   * Constructs a TrainDeparture object.
+   *
+   * @param trainNumber the number of the train
+   * @param departureTime the departure time of the train
+   * @param line the line the train operates on
+   * @param destination the destination of the train
+   */
   public TrainDeparture(int trainNumber, LocalTime departureTime, String line, String destination) {
     this.trainNumber = trainNumber;
     this.departureTime = departureTime;
@@ -21,5 +29,44 @@ public class TrainDeparture {
     this.destination = destination;
   }
 
+  public static LocalTime getCurrentTime() {
+    return currentTime;
+  }
+
+  public int getTrainNumber() {
+    return trainNumber;
+  }
+
+  public LocalTime getDepartureTime() {
+    return departureTime;
+  }
+
+  public String getLine() {
+    return line;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public int getTrack() {
+    return track;
+  }
+
+  public LocalTime getDelay() {
+    return delay;
+  }
+
+  public static void setCurrentTime(LocalTime currentTime) {
+    TrainDeparture.currentTime = currentTime;
+  }
+
+  public void setTrack(int track) {
+    this.track = track;
+  }
+
+  public void setDelay(LocalTime delay) {
+    this.delay = delay;
+  }
 
 }
