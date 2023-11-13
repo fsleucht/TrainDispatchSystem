dd4733efcc3c2b90a2075dcaf4875c6245fddd91
florianleucht <fsleucht@stud.ntnu.no>
2023-11-13 15:07:43 +0100

    added TrainDepartureRegistry

---
+++ src/main/java/edu/ntnu/stud/TrainDepartureRegistry.java
@@ -0,0 +1,78 @@
+package edu.ntnu.stud;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * This holds a registry of TrainDeparture objects.
+ *
+ * <p>It stores information about all TrainDeparture objects in a HashMap.
+ * It has methods for adding, searching and sorting TrainDeparture objects.
+ */
+public class TrainDepartureRegistry {
+  private final HashMap<Integer, TrainDeparture> trainDepartureMap;
+
+  public TrainDepartureRegistry() {
+    trainDepartureMap = new HashMap<>();
+  }
+
+  /**
+   * Creates a new TrainDeparture object and adds it to the registry.
+   * Does only add the TrainDeparture object if the train number does not already exist.
+   *
+   * @param trainNumber the number of the train
+   * @param hours the hour of departure
+   * @param minutes the minute of departure
+   * @param line the line the train operates on
+   * @param destination the destination of the train
+   * @throws IllegalArgumentException if the train number already exists.
+   */
+  public void addTrainDeparture(
+      int trainNumber, int hours, int minutes, String line, String destination)
+      throws IllegalArgumentException {
+    if (trainDepartureMap.containsKey(trainNumber)) {
+      throw new IllegalArgumentException("Train number already exists");
+    }
+    TrainDeparture trainDeparture =
+        new TrainDeparture(trainNumber, hours, minutes, line, destination);
+    trainDepartureMap.put(trainNumber, trainDeparture);
+  }
+
+  /**
+   * Searches for a TrainDeparture object with the given train number.
+   * If no TrainDeparture object is found, an empty Optional is returned.
+   *
+   * @param trainNumber the number of the train
+   * @return an Optional containing the TrainDeparture object with the given train number.
+   */
+  public Optional<TrainDeparture> searchTrainDeparture(int trainNumber) {
+    return Optional.ofNullable(trainDepartureMap.get(trainNumber));
+  }
+
+  /**
+   * Searches for TrainDeparture objects with the given Destination.
+   * If the destination is not found, an empty list is returned.
+   *
+   * @param destination the destination of the train
+   * @return a list of TrainDeparture objects with the given destination
+   */
+  public List<TrainDeparture> searchTrainDepartureDestination(String destination) {
+    return trainDepartureMap.values().stream()
+        .filter(trainDeparture -> trainDeparture.getDestination().equals(destination))
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * Sorts all TrainDeparture objects by departure time.
+   *
+   * @return a list of all TrainDeparture objects sorted by departure time
+   */
+  public List<TrainDeparture> getTrainDepartureSorted() {
+    return trainDepartureMap.values().stream()
+        .sorted(Comparator.comparing(TrainDeparture::getDepartureTime))
+        .collect(Collectors.toList());
+  }
+}
